bb22f1b9768594030731b03aa7119747605237a5
Leo6016 <54263518+Leo6016@users.noreply.github.com>
2022-12-18 17:40:58 -0500

    added func for chatbot (#27)
    * changed ml test
    
    * reverted to previous main
    
    * wrapped openai_test in func
    
    * modified file name

---
+++ web_app/chatbot.py
@@ -0,0 +1,41 @@
+import os
+import openai
+
+
+def get_response(message,targ):
+  openai.api_key = "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"
+  prompt="The following is a conversation in "+targ+" between two people.\n\nPerson1:"+message+"\n\nPerson2:"
+  response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt=prompt,
+  temperature=0.9,
+  max_tokens=150,
+  top_p=1,
+  frequency_penalty=0.0,
+  presence_penalty=0.0,
+  )
+  answer_targ=response["choices"][0]["text"].replace("\n","")
+  prompt="The following sentence is in "+targ+"\n\nPlease translate it into English, the translation should be literal, casual, and grammatically correct.\nSentence:"+answer_targ
+  response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt=prompt,
+  temperature=0.9,
+  max_tokens=150,
+  top_p=1,
+  frequency_penalty=0.0,
+  presence_penalty=0.0,
+  )
+  answer_eng=response["choices"][0]["text"].replace("\n","")
+  response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Correct this to standard English:\n\n"+answer_eng,
+  temperature=0,
+  max_tokens=60,
+  top_p=1.0,
+  frequency_penalty=0.0,
+  presence_penalty=0.0
+)
+  answer_eng=response["choices"][0]["text"].replace("\n","")
+  return answer_targ,answer_eng
+
+print(get_response("我是弱智","Chinese"))
