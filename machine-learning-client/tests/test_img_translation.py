255caa9e799f7b8f04e9445f08200843a2dbf789
DarrenLe20 <89809192+DarrenLe20@users.noreply.github.com>
2022-12-18 16:57:21 -0500

    test image read failure

--- machine-learning-client/tests/test_img_translation.py
+++ machine-learning-client/tests/test_img_translation.py
@@ -28,7 +28,7 @@ class Tests:
         exp2 = "Le renard brun rapide saute par-dessus le chien paresseux."
         exp3 = "Der schnelle braune Fuchs springt über den faulen Hund."
 
-        assert lang1 == (exp1 or exp1_1), "Expected Spanish translation to be correct!"
+        assert lang1 == exp1 or lang1 == exp1_1, "Expected Spanish translation to be correct!"
         assert lang2 == exp2, "Expected French translation to be correct!"
         assert lang3 == exp3, "Expected German translation to be correct!"
 
@@ -41,3 +41,24 @@ class Tests:
         text = pytesseract.image_to_string(img)
         expected = "Sample Text 3"
         assert text == expected, "Expected extracted text to be correct!"
+
+    def img_trans(self):
+        img = Image.open("machine-learning-client/tests/test.png")
+        text = pytesseract.image_to_string(img)
+        lang1 = translate.translate(text, "en", "es")
+        lang2 = translate.translate(text, "en", "fr")
+        lang3 = translate.translate(text, "en", "de")
+
+        exp1 = "Texto de muestra 3"
+        exp2 = "Exemple de texte 3 3"
+        exp3 = "Mustertext 3"
+
+        assert lang1 == exp1, "Expected Spanish translation to be correct!"
+        assert lang2 == exp2, "Expected French translation to be correct!"
+        assert lang3 == exp3, "Expected German translation to be correct!"
+
+    def no_img(self):
+        try:
+            img = Image.open("machine-learning-client/tests/no_test.png")
+        except:
+            assert True, "Expected to fail to open non-existent image!"
