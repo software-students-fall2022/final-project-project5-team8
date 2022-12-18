67bbd6028bc087e95ae71a4d5a9d03e90e753102
Leo6016 <gx324@nyu.edu>
2022-12-18 17:24:12 -0500

    wrapped openai_test in func

--- web_app/openai_test.py
+++ web_app/openai_test.py
@@ -1,14 +1,10 @@
 import os
 import openai
 
-openai.api_key = "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"
-message=""
-prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman:"
-while True:
-  message=input()
-  if message=="stop":
-    break
-  prompt=prompt+message+"\nAI:"
+
+def get_response(message,targ):
+  openai.api_key = "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"
+  prompt="The following is a conversation in "+targ+" between two people.\n\nPerson1:"+message+"\n\nPerson2:"
   response = openai.Completion.create(
   model="text-davinci-003",
   prompt=prompt,
@@ -16,9 +12,30 @@ while True:
   max_tokens=150,
   top_p=1,
   frequency_penalty=0.0,
-  presence_penalty=0.6,
-  stop=[" Human:", " AI:"]
-    )
-  answer=response["choices"][0]["text"]
-  print(answer)
-  prompt=prompt+answer+"\nHuman:"
+  presence_penalty=0.0,
+  )
+  answer_targ=response["choices"][0]["text"].replace("\n","")
+  prompt="The following sentence is in "+targ+"\n\nPlease translate it into English, the translation should be literal, casual, and grammatically correct.\nSentence:"+answer_targ
+  response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt=prompt,
+  temperature=0.9,
+  max_tokens=150,
+  top_p=1,
+  frequency_penalty=0.0,
+  presence_penalty=0.0,
+  )
+  answer_eng=response["choices"][0]["text"].replace("\n","")
+  response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Correct this to standard English:\n\n"+answer_eng,
+  temperature=0,
+  max_tokens=60,
+  top_p=1.0,
+  frequency_penalty=0.0,
+  presence_penalty=0.0
+)
+  answer_eng=response["choices"][0]["text"].replace("\n","")
+  return answer_targ,answer_eng
+
+print(get_response("我是弱智","Chinese"))
